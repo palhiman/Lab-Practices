b87da76f2c46488dc7ba9cc53e884064373358b6
palhiman <palhimanshu786@gmail.com>
2020-02-25 00:26:38 +0530

    learning javascript

---
+++ JS/index.js
@@ -0,0 +1,18 @@
+// I am learning Javascript
+/* It is never late in learning.
+ * you just need inspiration to go on 
+ * and find yourself a nice and new things to learn.
+ */
+
+var myName = "Himanshu"
+console.log(myName)
+
+
+var arr = ["himanshu"];
+arr.push("Shekhar","Pal");
+console.log(arr)
+
+var arr2 = [["Himanshu", 24], ["Chhavi", 24], ["Shekhar", 24]];
+var x = arr2.pop()
+console.log(arr2)
+console.log(x)
