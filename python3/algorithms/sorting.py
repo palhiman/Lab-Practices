b148b79f0ccd647e3bed459560db1092d2547845
palhiman <palhimanshu786@gmail.com>
2020-04-19 01:26:10 +0530

    Sorting algorithms implementation in Python3

---
+++ python3/algorithms/sorting.py
@@ -0,0 +1,22 @@
+from random import randint
+from timeit import repeat
+
+def run_sorting_algorithm(algorithm, array):
+    setup_code = f"from __main__ import {algorithm}" \
+        if algorithm != "sorted" else ""
+
+    stmt = f"{algorithm}({array})"
+
+    times = repeat(setup_code, stmt=stmt, repeat=3, number=10)
+
+    print(f"Algorithm:{algorithm}. Minimum execution time :{min(times)})
+
+
+
+ARRAY_LENGTH = 10000
+if __name__ == "__main__":
+
+
+    array = [randint(0, 1000) for i in range(ARRAY_LENGTH)]
+
+    run_sorting_algorithm(algorithm="sorted", array=array)
