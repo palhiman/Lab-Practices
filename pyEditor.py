4ef306af2207945bb0905f6caa82943d1db16f42
palhiman <palhimanshu786@gmail.com>
2021-11-25 22:40:06 +0530

    Text editor updated

---
+++ pyEditor.py
@@ -0,0 +1,60 @@
+################################################
+# Author: Himanshu Shekhar, Chhavi Parikh...
+# Date: 20/3/2017
+# Objective : This project shows a simple text editor developed using python version3
+# and tkinter (python GUI tool) with open and save functionality.
+################################################
+
+import tkinter as tk
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+
+# function to open file. This opens dialog box
+def open_file():
+    """Open a file for editing."""
+    filepath = askopenfilename(
+        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
+    )
+    if not filepath:
+        return
+    txt_edit.delete(1.0, tk.END)
+    with open(filepath, "r") as input_file:
+        text = input_file.read()
+        txt_edit.insert(tk.END, text)
+    window.title(f"File Location- {filepath}")
+
+# function to save file.
+def save_file():
+    """Save the current file as a new file."""
+    filepath = asksaveasfilename(
+        defaultextension="txt",
+        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")],
+    )
+    if not filepath:
+        return
+    with open(filepath, "w") as output_file:
+        text = txt_edit.get(1.0, tk.END)
+        output_file.write(text)
+    window.title(f"File Location - {filepath}")
+
+
+# creating GUI using tkinter
+window = tk.Tk()
+window.title("Shadow03")
+window.rowconfigure(0, minsize=800, weight=1)
+window.columnconfigure(1, minsize=800, weight=1)
+
+txt_edit = tk.Text(window)
+fr_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
+btn_open = tk.Button(fr_buttons, text="Open", command=open_file)
+btn_save = tk.Button(fr_buttons, text="Save", command=save_file)
+
+btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+btn_save.grid(row=1, column=0, sticky="ew", padx=5)
+
+fr_buttons.grid(row=0, column=0, sticky="ns")
+txt_edit.grid(row=0, column=1, sticky="nsew")
+
+# ends the window loop.
+window.mainloop()
+
+
